fb2cda5e9ccc5231e8d24c71d53a94a7a4e73bf0
Triston <tarmstrong95@proton.me>
2024-04-07 11:50:25 -0500

    (feat): abstract recommended radio card

---
+++ src/components/recommended_radio/index.tsx
@@ -0,0 +1,7 @@
+export function RecommendedRadio() {
+  return (
+    <div id="bottom-r" className="font-bold p-2 bg-white flex-1 rounded-xl  shadow-md">
+      <h2>Recommended For You</h2>
+    </div>
+  )
+}
