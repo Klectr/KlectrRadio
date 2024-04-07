4e9bb74114770cd7e0c06e319169a908bb47fdda
Triston <tarmstrong95@proton.me>
2024-04-07 11:50:11 -0500

    (feat): abstract popular card

---
+++ src/components/popular/index.tsx
@@ -0,0 +1,10 @@
+export function Popular() {
+  return (
+    <div id="middle" className="bg-gray-300 flex-[.8] rounded-xl flex flex-col max-h-[150px]">
+      <div id="middle-main" className="font-bold p-2 bg-white flex-1 rounded-xl  shadow-md">
+        <h2>Popular</h2>
+      </div>
+    </div>
+  )
+}
+
