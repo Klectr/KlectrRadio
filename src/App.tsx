aeefde761a4f51e4023a216cc05c05f49fa724da
Triston <tarmstrong95@proton.me>
2024-04-24 18:15:57 -0400

    style: add widths and heights to various components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "kaioken"
 import { useStorage } from "./hooks/storageStores"
 import { useStationsStore } from "./hooks/stationStores"
-import { MusicPlayer, Popular, LocalRadio, RecommendedRadio, Navigation } from "./components"
+import { Popular, Navigation } from "./components"
 
 export function App() {
   const { getStationsFile } = useStorage()
@@ -32,14 +32,15 @@ export function App() {
 
 
       {/* Right Section __________*/}
-      <div className="flex-1 min-h-[98vh] max-h-[98vh] flex h-0 p-2">
+      <div className="flex-1 min-h-[98vh] max-h-[98vh] flex h-0 p-2 min-w-0">
         <div className="bg-gray-200 w-full border rounded-xl p-4 flex-col flex gap-4">
-          <MusicPlayer />
+          {/*<MusicPlayer />*/}
           <Popular />
           {/* Bottom Section __________*/}
-          <div id="bottom" className="flex-1 rounded-xl flex flex-row gap-4">
-            <LocalRadio />
-            <RecommendedRadio />
+
+          <div id="bottom" className="flex-1 rounded-xl flex flex-row gap-4 bg-white">
+            {/*<LocalRadio />*/}
+            {/*<RecommendedRadio />*/}
           </div>
         </div>
       </div>
